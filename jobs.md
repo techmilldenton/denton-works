cb572e809c0228195742e1211ceb2a3e95fc31cd
Kyle Taylor <kyletaylored@gmail.com>
2020-03-30 16:07:41 -0500

    Update pages

---
+++ jobs.md
@@ -0,0 +1,14 @@
+---
+title: Find a Job
+---
+
+<div id="jobs">
+
+  <input class="search" placeholder="Search" />
+  <button class="sort" data-sort="name">
+    Sort
+  </button>
+
+  <ul class="list"></ul>
+
+</div>
--- js/sheet.js
+++ js/sheet.js
@@ -1,25 +1,46 @@
-
+// Init
+var window = window || {};
 var publicSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/1iCpYHV3MeGKyOlSjih4wFuhysAbEfgENLPpkJCO5jIs/edit?usp=sharing";
 
 function showInfo(data) {
+    window.sheetData = data;
   // do something with the data
   console.log(data);
 //   console.log(JSON.stringify(data, null, 2));
+
     var keys = Object.keys(data.candidates.elements[0]).map(key => {
         console.log(key);
         key = processCandidateKey(key);
         return Object.assign({}, {"title": key});
     });
+
     var values = [];
     data.candidates.elements.forEach((el, i) => {
         console.log(el, i)
         values.push(Object.values(el));
     });
+
+    // Datatable
+    var $candidates = jQuery('#candidates');
+    console.log($candidates);
+    if ($candidates.length > 0) {
+        // $candidates.DataTable( {
+        //     data: values,
+        //     columns: keys
+        // } );
+    }
+
+    // List
+    keys = {};
+    Object.keys(data.candidates.elements[0]).forEach(key => {
+        console.log(key);
+        key = processCandidateKey(key);
+        key = key.toLowerCase();
+        var keyId = key.split("0").slice(0,2).join("-");
+        keys[keyId] = key;
+    });
     console.log(keys);
-    jQuery('#candidates').DataTable( {
-        data: values,
-        columns: keys
-    } );
+    processCandidateList(keys, values, 'candidates');
 }
 
 /**
@@ -44,6 +65,26 @@ function processCandidateValue(val, index) {
     }
 
     return key;
+}
+
+function processCandidateList(keys, values, id) {
+    var options = {
+        valueNames: keys,
+        // Since there are no elements in the list, this will be used as template.
+        item: '<li><h3 class="name"></h3><p class="born"></p></li>',
+        item: `<div class="col-md-6 col-lg-3">
+                <div class="single_candidates text-center">
+                    <div class="thumb">
+                        <img src="img/candiateds/3.png" alt="">
+                    </div>
+                    <a href="#"><h4>Markary Jondon</h4></a>
+                    <p>Software Engineer</p>
+                </div>
+            </div>`
+      };
+      
+      var userList = new List(id, options, values);
+      
 }
 
 //initialise and kickstart the whole thing.
