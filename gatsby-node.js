ece7e79d9591be1069d39b7ddcd6162ef6a94fcb
newswim <ofenixculpa@gmail.com>
2020-04-03 13:07:22 -0500

    rebuild docs folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const path = require('path')
-const fs = require('fs')
+// const fs = require('fs')
 
-exports.onPreInit = () => {
-  if (process.argv[2] === 'build') {
-    fs.rmdirSync(path.join(__dirname, 'docs'), { recursive: true })
-    fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'public_dev'))
-  }
-}
+// exports.onPreInit = () => {
+//   if (process.argv[2] === 'build') {
+//     fs.rmdirSync(path.join(__dirname, 'docs'), { recursive: true })
+//     fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'public_dev'))
+//   }
+// }
 
-exports.onPostBuild = () => {
-  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'))
-  fs.renameSync(path.join(__dirname, 'public_dev'), path.join(__dirname, 'public'))
-}
+// exports.onPostBuild = () => {
+//   fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'))
+//   fs.renameSync(path.join(__dirname, 'public_dev'), path.join(__dirname, 'public'))
+// }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
