8775ea34b664873274fe42d924d123c48ef4f397
Dan Minshew <ofenixculpa@gmail.com>
2020-04-05 20:54:42 -0500

    Clean up the gsheet library

--- src/getSheets.ts
+++ src/getSheets.ts
@@ -3,52 +3,39 @@ import { buildIndexUrl, buildSheetUrl } from './utils'
 
 const GST_PREFIX = /^gsx\$/
 
-// const key = '1iCpYHV3MeGKyOlSjih4wFuhysAbEfgENLPpkJCO5jIs'
-
-// const worksheetUrl = buildIndexUrl(key)
-
-const getIdAndTitle = (c: { id: string; title: string }) => ({
-  id: c.id.split('/').reverse()[0],
-  title: c.title,
-})
-
 /**
  *
- * @param sheet simple getter
- * TODO: add a fallback if get fails
- */
-const getEntries = (sheet: SheetV1) => sheet.data.feed.entry
-
-/**
- *
- * @param rawEntry entire row, including metadata
- * @returns an array of allowed keys corresponding to table headings
+ * @param rawEntry    entire row, including metadata
+ * @returns           array of allowed keys corresponding to table headings
  */
 const allowedKeys = (rawEntry: Record<string, { $t: string }>) =>
   Object.keys(rawEntry).filter(e => GST_PREFIX.test(e))
 
 /**
  *
- * @param keys an array of dirty keys, (ex. `gsx$keyname`)
+ * @param keys        array of dirty keys, (ex. `gsx$keyname`)
+ * @returns           array of cleaned keys, (ex. `keyname`)
  */
 const stripKeys = (keys: Array<string>) => keys.map(k => k.replace(GST_PREFIX, ''))
 
 /**
  *
- * @param ids an array of sheet id's
- * @returns an array of promises for each id
+ * @param ids         array of sheet id's
+ * @returns           array of promises for each id
  */
 const getSheets = (ids: Array<string>, key: string) => {
   const urls = ids.map(id => buildSheetUrl(key, id))
 
-  return axios.all(urls.map(id => axios.get(id)))
+  return axios.all<SheetV1>(urls.map(id => axios.get(id)))
 }
 
 /**
+ * The "worker" function. It returns a cleaned set of
+ * key/value pairs (tuples), based on supplied worksheet rows.
  *
- * @param entries correspond to a row
- * @param allowed keys
- * @param cleaned keys
+ * @param entries     correspond to a row
+ * @param allowed     array of keys to keep
+ * @param cleaned     keys that are stripped of their `gsx$` prefix
  */
 const process = ({
   entries,
@@ -69,43 +56,46 @@ const process = ({
 }
 
 /**
- * @param key a googlesheet worksheet key
- * @example '1iCpYHV3MeGKyOlSjih4wFuhysAbEfgENLPpkJCO5jIs'
+ * Uses axios to fetch data from a given worksheet key.
+ * Runs subsequent fetch calls on returned sheet id's.
+ *
+ * @param key         a worksheet key
+ * @example           '1iCpYHV3MeGKyOlSjih4wFuhysAbEfgENLPpkJCO5jIs'
+ * @param onSuccess   callback function for success case
+ * @param onFailure   callback function for failure case
  */
 export const getData = (
   key: string,
-  cb: (
+  onSuccess: (
     o: {
       id: string
       title: string
       entries: [string, string][][]
     }[]
-  ) => void
+  ) => void,
+  onFailure: (e: Error) => void
 ) =>
   axios
     .get(buildIndexUrl(key))
     .then((sheet: WorkSheetV1) => {
-      const info = sheet.data.feed.entry
-        .map(f => ({ id: f.id.$t, title: f.title.$t }))
-        .map(getIdAndTitle)
+      /**
+       * Build up a record of metadata
+       */
+      const info = sheet.data.feed.entry.map(f => ({
+        id: f.id.$t.split('/').reverse()[0],
+        title: f.title.$t,
+      }))
 
-      // console.log('info:', info)
       return info
     })
-    .then((info: Array<Info>) => {
+    .then(info => {
       const ids = info.map(i => i.id)
       const promisedSheets = getSheets(ids, key)
 
-      // console.log('promisedSheets', promisedSheets)
-
       promisedSheets.then(p => {
-        const entries = p.map(getEntries)
+        const entries = p.map(sheet => sheet.data.feed.entry)
         const allowed = entries.map(e => allowedKeys(e[0]))
         const cleaned = allowed.map(stripKeys)
-
-        // console.log('entries', entries)
-        // console.log('allowed', allowed)
-        // console.log('cleaned', cleaned)
 
         const o = info.map((n, i) => {
           return {
@@ -119,28 +109,36 @@ export const getData = (
           }
         })
 
-        cb(o)
+        onSuccess(o)
       })
     })
-    .catch(e => {
+    .catch((e: Error) => {
       console.error(e)
+      onFailure(e)
     })
 
+/**
+ * TODO: This is a very incomplete type
+ */
 type Entries = Array<{ [k: string]: { $t: string } }>
 
+/**
+ * TODO: This is a very incomplete type
+ */
 type WorkSheetV1 = {
   data: {
     feed: {
+      /**
+       * The only fields I really need are id and title
+       */
       entry: Array<{ id: { $t: string }; title: { $t: string } }>
     }
   }
 }
 
-type Info = {
-  id: string
-  title: string
-}
-
+/**
+ * TODO: This is a very incomplete type
+ */
 type SheetV1 = {
   data: {
     feed: {
