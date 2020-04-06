cfb2957f654cba8ff456c2c2944cfe46d7ebeee6
Kyle Taylor <kyletaylored@gmail.com>
2020-04-06 16:11:54 -0500

    Update job listing

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -7,25 +7,83 @@ import { sheetId } from '../constants'
 import { getData } from '../getSheets'
 import { fromEntries } from '../utils'
 import { Loader } from '../components/Loader'
+import '../styles/job.scss'
 
 const renderJob = (j: Job, key: number) => (
-  <div key={key}>
-    <div>Name: {j.companyname}</div>
-    <div>Address: {j.address}</div>
-    <div>Email: {j.emailaddress}</div>
-    <div>Description: {j.jobdescription}</div>
-    {j.dentonchamberofcommercemember && <div>Chamber: ✅</div>}
-    <div>Job Title: {j.jobtitle}</div>
-    <div>Phone: {j.phonenumber ? j.phonenumber : <>No ☎️ Provided!</>}</div>
-    {/* <div>timestamp: {j.timestamp}</div> */}
-    <div>Type of Work: {j.typeofwork}</div>
-    {j.websiteurl && (
-      <div>
-        Website: <a href={j.websiteurl}>{j.websiteurl}</a>
+  <div key={key} className={'job job-' + key}>
+    <div className="top">
+      <div className="time">added {j.timestamp} ago</div>
+      <div className="category">{j.jobcategory}</div>
+    </div>
+    <div className="middle">
+      <div className="col-left">
+        <h4 className="title">
+          {j.jobtitle}
+          <span className="type">{j.typeofwork}</span>
+        </h4>
+        <div className="description">{j.jobdescription}</div>
       </div>
-    )}
-    <br />
-    <br />
+      <div className="col-right">
+        <div className="contact-wrapper">
+          <a href={'mailto:' + j.emailaddress + '?subject=' + j.jobtitle} className="btn">
+            Contact
+          </a>
+        </div>
+        <div className="rate-wrapper">
+          <span className="rate">{j.jobrate}</span>
+        </div>
+        <div className="alt-icons">
+          <div className="phone">
+            <div>Phone: {j.phonenumber ? j.phonenumber : <>No ☎️ Provided!</>}</div>
+          </div>
+          {j.jobpostlink ? (
+            <div className="post-link">
+              <a href={j.jobpostlink}>Online Link</a>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+    </div>
+    <div className="bottom">
+      <div className="company">
+        <div className="name">
+          {j.companyname}
+          {j.dentonchamberofcommercemember && (
+            <div className="coc">
+              <img
+                src="https://cdn.jsdelivr.net/gh/techmilldenton/denton-works@master/src/images/dcoc-logo.svg"
+                alt="Denton Chamber of Commerce Member"
+                title="Denton Chamber of Commerce Member"
+              ></img>
+            </div>
+          )}
+        </div>
+        <div className="links">
+          {j.websiteurl ? (
+            <a target="_blank" href={j.websiteurl}>
+              🔗
+            </a>
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+      <div className="address">
+        <a
+          target="_blank"
+          href={
+            'https://www.google.com/maps/search/?api=1&query=' +
+            j.companyname.replace(/\s/g, '+') +
+            '+' +
+            j.address.replace(/\s/g, '+')
+          }
+        >
+          {j.address}
+        </a>
+      </div>
+    </div>
   </div>
 )
 
@@ -74,6 +132,9 @@ type Job = {
   jobtitle: string
   jobdescription: string
   typeofwork: string
+  jobcategory: string
+  jobrate: string
+  jobpostlink: string
 }
 
 type Jobs = Array<Array<[string, string]>>
